41e503bdb1cc1c8b23fe3218f194aebc9933f1bb
Sergio Centellas <sergio.ko@icloud.com>
2022-08-02 13:42:45 +0200

    [MOD] Working on Shopping cart service storage

--- onebox-technical-test/src/app/core/services/concert-api.service.ts
+++ onebox-technical-test/src/app/core/services/concert-api.service.ts
@@ -3,10 +3,18 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ConcertApiService {
+  public selectedDates: any;
+
   constructor(private http: HttpClient) {}
 
   /**
    * Get all concerts
    */
   public getEvents = () => this.http.get('http://localhost:3000/concerts');
+
+  /**
+   * Get specific event
+   */
+  public getEventInfo = (eventID: string | null) =>
+    this.http.get('http://localhost:3000/' + eventID);
 }
--- onebox-technical-test/src/app/modules/pages/shopping-cart/shopping-cart.component.ts
+++ onebox-technical-test/src/app/modules/pages/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ConcertApiService } from 'src/app/core/services/concert-api.service';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -6,7 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-cart.component.scss'],
 })
 export class ShoppingCartComponent implements OnInit {
-  constructor() {}
+  public eventInfo: any;
+  public availableEvent: boolean = true;
+  public availableInfo = [68, 184];
 
-  ngOnInit(): void {}
+  constructor(
+    private concertService: ConcertApiService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const eventID = this.route.snapshot.paramMap.get('id');
+
+    this.availableInfo.includes(+eventID!)
+      ? this.getEventInfo(eventID)
+      : (this.availableEvent = false);
+  }
+
+  public getEventInfo(eventID: string | null): void {
+    this.concertService.getEventInfo(eventID).subscribe((res: any) => {
+      this.eventInfo = res[0];
+      this.eventInfo.sessions.sort((a: any, b: any) =>
+        a.date > b.date ? 1 : -1
+      );
+      console.log('TEST: eventInfo => ', this.eventInfo);
+    });
+  }
 }
